79ccd02c74104d70cb44edf65a2923d923429092
Jeison <jeison.aoalves@gmail.com>
2025-08-30 07:29:29 -0300

    Initial commit Creating main loop Connecting Menu and Game classes Create assets folder Insert asserts Create Menu: Background, sound and fonts Create the const file

---
+++ code/Const.py
@@ -0,0 +1,19 @@
+# W
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+
+#C
+COLOR_ORANGE = (255, 165, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLUE = (0, 0, 255)
+COLOR_RED = (255, 0, 0)
+COLOR_YELLOW = (255, 255, 0)
+COLOR_CYAN = (0, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
---
+++ code/Menu.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame
+from pygame.font import Font
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.Const import COLOR_BLUE, WIN_WIDTH, COLOR_YELLOW, MENU_OPTION, COLOR_WHITE
+
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha() #select the menu image
+        self.surf = pygame.transform.scale(self.surf, (800, 600))#adjust the image for the window size
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def run(self):
+        pygame.mixer_music.load('./asset/Menu.wav')#select the music for the menu
+        pygame.mixer_music.play(-1) #play the music and -1 is to repeat
+        while True:
+            # DRAW IMAGES
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(150, "SKY", COLOR_BLUE, ((WIN_WIDTH / 2), 100))
+            self.menu_text(150, "FIRE", COLOR_BLUE, ((WIN_WIDTH / 2), 220))
+
+            for i in range(len(MENU_OPTION)):
+                    self.menu_text(50, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 430 + 35 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close Window
+                    quit()  # end pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Chiller", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
